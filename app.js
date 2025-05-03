54b470db7589c122281de87ad0fe504ffa1a1226
LivingtonMerello98 <livington.merello@gmail.com>
2025-05-03 17:29:29 +0200

    [UPD] set routes

--- app.js
+++ app.js
@@ -25,35 +25,9 @@ const openai = new OpenAI({
 
 
 //rute /endpoint / url
-app.post('/api/translate', async (req, res) => {
-
-    //prompt def
-    const { text, targetLang } = req.body
-    //def ruolo
-    const promptSystem1 = "Traduttore professionale.";
-    const promptSystem2 = "Rispondi solo con la traduzione. Niente altro.";
-    const promptUser = `Traduci in ${targetLang}: ${text}`;
-    //call openIA
-    try {
-        const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-                { role: "system", content: promptSystem1 },
-                { role: "system", content: promptSystem2 },
-                { role: "user", content: promptUser },
-            ],
-            max_tokens: 500,
-            response_format: { type: 'text' }
-        })
-
-        const translateText = completion.choices[0].message.content;
-        return res.status(200).json({ translateText });
-    } catch (error) {
-        console.log(err);
-        return res.status(500).json({ error: "errore di traduzione" });
-    }
-});
-
+app.post('/api/chatbot/', async (req, res) => {
+    return res.json({ message: 'server on' });
+})
 //serv backend
 app.listen(PORT, () => {
     console.log('server running on port: ' + PORT);
