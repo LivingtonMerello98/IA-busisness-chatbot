66f714b89bd7e82625b8662cb19703361fe9f004
LivingtonMerello98 <livington.merello@gmail.com>
2025-05-03 17:42:49 +0200

    [UPD] set prompt context for Openai

--- app.js
+++ app.js
@@ -26,7 +26,20 @@ const openai = new OpenAI({
 
 //rute /endpoint / url
 app.post('/api/chatbot/', async (req, res) => {
-    return res.json({ message: 'server on' });
+    //contesto da mandare a Openai
+    const context = `Sei un assistente di supporto per un supermercato 'Esselunga'
+                    informazioni sul supermercato:
+                    -Ubicazione: Duomo numero 77, Milano
+                    -Orario: da lunedì a sabato dalle 9 alle 21
+                    -Prodotti: Pane, Latte, Uova, Frutta, Verdura, Carne e bibite
+                    -Marchi: Mulino Bianco, Parmalat, Dole, Cocacola, Sprite
+                    -Metodi di Pagamento: Cash, Carta
+                Puoi solo rispondere a domande inerenti all'attività e i prodotti. risposte altre domande sono vietate`
+    //return res.json({ message: 'server on' });
+
+    //domanda user
+    //richiesta a openai
+    //restituzione risposta a user
 })
 //serv backend
 app.listen(PORT, () => {
